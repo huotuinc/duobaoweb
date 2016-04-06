0f3947f87f832926529a4331e4325df82522495e
zdq1987 <345833900@qq.com>
2016-04-06 14:40:12 +0800

    测试跳转到商品活动首页 测试通过商品ID跳转到商品详情页

--- src/test/java/com/huotu/duobaoweb/controller/GoodsControllerTestJumpToGoodsActivityDetailByGoodsId.java
+++ src/test/java/com/huotu/duobaoweb/controller/GoodsControllerTestJumpToGoodsActivityDetailByGoodsId.java
@@ -1,6 +1,5 @@
 package com.huotu.duobaoweb.controller;
 
-import com.huotu.duobaoweb.base.BaseTest;
 import com.huotu.duobaoweb.common.CommonEnum;
 import com.huotu.duobaoweb.entity.Goods;
 import com.huotu.duobaoweb.entity.Issue;
@@ -12,29 +11,28 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Created by daisy.zhang on 2016/3/30.
- * 测试通过商品ID，跳转到商品活动首页
+ * Created by daisy.zhang on 2016/4/6.
  */
-
-@Transactional
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-public class GoodsControllerTestJumpToGoodsActivityIndex extends BaseTest {
+@ActiveProfiles("development")
+@Transactional
+public class GoodsControllerTestJumpToGoodsActivityDetailByGoodsId {
     @Autowired
     MockMvc mockMvc;
     @Autowired
@@ -56,7 +54,7 @@ public class GoodsControllerTestJumpToGoodsActivityIndex extends BaseTest {
         mockIssue.setStepAmount(mockGoods.getStepAmount());//单次购买最低量
         mockIssue.setDefaultAmount(mockGoods.getDefaultAmount()); //缺省购买人次
         mockIssue.setToAmount(mockGoods.getToAmount()); //总需购买人次
-        mockIssue.setBuyAmount(1L); //已购买的人次
+        mockIssue.setBuyAmount(2L); //已购买的人次
         mockIssue.setPricePercentAmount(mockGoods.getPricePercentAmount()); //每人次单价
         mockIssue.setAttendAmount(mockGoods.getAttendAmount()); //购买次数,在中奖时从每期中累计此值
         mockIssue.setStatus(CommonEnum.IssueStatus.going);//状态
@@ -87,121 +85,109 @@ public class GoodsControllerTestJumpToGoodsActivityIndex extends BaseTest {
         mockGoods = mockGoodsRep.save(mockGoods);
     }
 
-    //  GoodID不传，错误码和错误信息还未定义
+    //商品ID不传,判断容错（错误码还未定义，定义后统一修改）
     @Test
-    public void TestNoGoodsID() throws Exception {
-        mockMvc.perform(get("/index").param("goodsId", ""))
+    public void TestNOGoodsID() throws Exception {
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", ""))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(1001)))
                 .andExpect((jsonPath("$.resultData.resultDescription").value("参数错误")));
     }
 
-    //上传GoodID格式错误
+    //商品ID错误,判断容错（错误码还未定义，定义后统一修改）
     @Test
     public void TestWrongGoodsID() throws Exception {
-        mockMvc.perform(get("/index").param("goodsId", ""))
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", ""))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(1001)))
                 .andExpect((jsonPath("$.resultData.resultDescription").value("参数错误")));
     }
 
-    //测试商品ID在数据库中不存在，错误码和错误信息还未定义
+    //商品ID在数据库中不存在,判断容错（错误码还未定义，定义后统一修改）
     @Test
     public void TestNotFindGoodsID() throws Exception {
-        mockMvc.perform(get("/index").param("goodsId", "-1"))
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", "999999999"))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(1002)))
                 .andExpect((jsonPath("$.resultData.resultDescription").value("商品未找到")));
     }
 
-    //测试商品状态未审核，错误码和错误信息暂未定义
+    //商品状态未审核,判断容错（错误码还未定义，定义后统一修改）
     @Test
     public void TestGoodUnCheck() throws Exception {
         mockGoods.setStatus(CommonEnum.GoodsStatus.uncheck);
-        mockMvc.perform(get("/index").param("goodsId", mockGoods.getId().toString()))
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", mockGoods.getId().toString()))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(10009)))
                 .andExpect((jsonPath("$.resultData.Msg").value("商品未审核")));
 
     }
 
-    //测试商品状态下架，错误码和错误信息暂未定义
-    @Test
-    public void TestGoodsDown() throws Exception {
-        mockGoods.setStatus(CommonEnum.GoodsStatus.down);
-        mockMvc.perform(get("/index").param("goodsId", mockGoods.getId().toString()))
-                .andExpect(status().isOk())
-                .andExpect((jsonPath("$.resultData.Code").value(10010)))
-                .andExpect((jsonPath("$.resultData.Msg").value("商品已下架")));
-
-    }
-
-    //测试商品已过截止期，错误码和错误信息暂未定义
+    //商品已经过期,判断容错（错误码还未定义，定义后统一修改）
     @Test
     public void TestExpired() throws Exception {
         mockGoods.setStatus(CommonEnum.GoodsStatus.up);
         mockGoods.setEndTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2016-03-27 00:00:00"));
-        mockMvc.perform(get("/index").param("goodsId", mockGoods.getId().toString()))
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", mockGoods.getId().toString()))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(10011)))
                 .andExpect((jsonPath("$.resultData.Msg").value("商品已过期")));
     }
 
-    //测试商品活动还未开始，错误码和错误信息暂未定义
+    //商品还未开始活动,判断容错（错误码还未定义，定义后统一修改）
     @Test
     public void TestNotStart() throws Exception {
         mockGoods.setStatus(CommonEnum.GoodsStatus.up);
         mockGoods.setStartTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2018-03-27 00:00:00"));
         mockGoods.setEndTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2019-03-27 00:00:00"));
-        mockMvc.perform(get("/index").param("goodsId", mockGoods.getId().toString()))
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", mockGoods.getId().toString()))
                 .andExpect(status().isOk())
                 .andExpect((jsonPath("$.resultData.Code").value(10012)))
                 .andExpect((jsonPath("$.resultData.Msg").value("商品活动还未开始")));
     }
 
-    //测试商品ID正确，活动处于进行中，用户未登陆状态
+    //商品活动开始，但还未有任何人参与，判断接口返回模型数据是否正确
     @Test
-    public void TestUserNotLogin() throws Exception {
-        //原价
-        BigDecimal costPrice = mockGoods.getPricePercentAmount().multiply(new BigDecimal(mockGoods.getToAmount()));
-        //现价
-        BigDecimal currentPrice = mockGoods.getPricePercentAmount().multiply(new BigDecimal(mockGoods.getStepAmount()));
-        mockGoods.setEndTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2018-03-27 00:00:00"));
-        mockMvc.perform(get("/index").param("goodsId", mockGoods.getId().toString()))
+    public void TestNoJoined() throws Exception {
+        mockGoods.setStatus(CommonEnum.GoodsStatus.up);
+        mockGoods.setStartTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2016-03-27 00:00:00"));
+        mockGoods.setEndTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2019-03-27 00:00:00"));
+        mockIssue.setBuyAmount(0L);
+        mockMvc.perform(get("/detailByGoodsId").param("goodsId", mockGoods.getId().toString()))
                 .andExpect(status().isOk())
-                .andExpect((jsonPath("$.resultData.Code").value(1)))
-                .andExpect((jsonPath("$.resultData.data.id").value(mockGoods.getId().toString())))
-                .andExpect((jsonPath("$.resultData.data.defaultPictureUrl").value(mockGoods.getDefaultPictureUrl())))
-                .andExpect((jsonPath("$.resultData.data.costPrice").value(costPrice)))
-                .andExpect((jsonPath("$.resultData.data.currentPrice").value(currentPrice)))
-                .andExpect((jsonPath("$.resultData.data.startTime").isNotEmpty()))
-                .andExpect((jsonPath("$.resultData.data.endTime").isNotEmpty()))
-                .andExpect((jsonPath("$.resultData.data.joinCount").value(false)))
-                .andExpect((jsonPath("$.resultData.data.joined").value(false)))
-                .andExpect((jsonPath("$.resultData.data.issueId").isEmpty()));
+                .andExpect((jsonPath("$.resultData.Code").value(10012)))
+                .andExpect((jsonPath("$.resultData.data.id").value(mockGoods.getId())))
+                .andExpect((jsonPath("$.resultData.data.issueId").value(mockGoods.getIssue())))
+                .andExpect((jsonPath("$.resultData.data.pictureUrls").isNotEmpty()))
+                .andExpect((jsonPath("$.resultData.data.status").value(0)))
+                .andExpect((jsonPath("$.resultData.data.title").value(mockGoods.getTitle())))
+                .andExpect((jsonPath("$.resultData.data.progress").value(mockIssue.getBuyAmount() / mockGoods.getToAmount())))
+                .andExpect((jsonPath("$.resultData.data.toAmount").value(mockIssue.getToAmount())))
+                .andExpect((jsonPath("$.resultData.data.remainAmount").value(mockIssue.getToAmount() - mockIssue.getBuyAmount())))
+                .andExpect((jsonPath("$.resultData.data.defaultAmount").value(mockIssue.getDefaultAmount())))
+                .andExpect((jsonPath("$.resultData.data.stepAmount").value(mockIssue.getStepAmount())))
+                .andExpect((jsonPath("$.resultData.data.joinCount").value(mockIssue.getBuyAmount())))
+                .andExpect((jsonPath("$.resultData.data.numbers").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.fullPrice").value(mockIssue.getPricePercentAmount().multiply(new BigDecimal(mockIssue.getToAmount())))))
+                .andExpect((jsonPath("$.resultData.data.toAwardTime").isNotEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardUserName").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardUserCityName").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardUserIp").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardUserJoinCount").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardTime").value(mockIssue.getAwardingDate())))
+                .andExpect((jsonPath("$.resultData.data.luckNumber").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.awardUserHead").isEmpty()))
+                .andExpect((jsonPath("$.resultData.data.firstBuyTime").isEmpty()));
+    }
+
+    //商品活动有用户参与过，查看模型返回数据是否正确
+    @Test
+    public void TestUserJoined() {
 
     }
 
-
-//    //测试商品ID正确，活动进行中，用户已登陆，但是未购买
-//    @Test
-//    public void TestUserLoginNotBuy() throws Exception {
-//        //原价
-//        BigDecimal costPrice = mockGoods.getPricePercentAmount().multiply(new BigDecimal(mockGoods.getToAmount()));
-//        //现价
-//        BigDecimal currentPrice = mockGoods.getPricePercentAmount().multiply(new BigDecimal(mockGoods.getStepAmount()));
-//        mockUser = generateUserWithMobileWithToken("123456", mockUserRep);
-//
-//
-//
-//    }
-//    //测试商品ID正确，活动进行中，用户已登陆，并且购买过
-//    @Test
-//    public void TestUserLoginAndBuy() throws UnsupportedEncodingException {
-//        mockUser = generateUserWithMobileWithToken("123456", mockUserRep);
-//
-//    }
-    //测试商品ID正确，状态未审核，错误码和错误信息暂未定义
+    //商品状态等待开奖
+    //商品状态已开奖
 
 
 }
