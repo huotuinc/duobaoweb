4013161ca9b5178ec4e7cc3114044195fd668b4b
XHKJOE <1092181938@qq.com>
2016-04-14 20:19:08 +0800

    修正资源

--- duobao-web/src/main/java/com/huotu/mallduobao/service/impl/LocalStaticResourceService.java
+++ duobao-web/src/main/java/com/huotu/mallduobao/service/impl/LocalStaticResourceService.java
@@ -38,9 +38,8 @@ public class LocalStaticResourceService extends AbstractStaticResourceService {
         File file = new File(context.getServletContext().getRealPath("/"));
         this.fileHome = file.toURI();
         String url=System.getProperty("user.dir");
-        StringBuilder stringBuilder = new StringBuilder("http://192.168.3.30:8888");//http://sis.51funmore.com:8081
-        StringBuilder stringBuilder = new StringBuilder("http://localhost:8080");//http://sis.51funmore.com:8081
-        stringBuilder.append(context.getServletContext().getContextPath());
+        StringBuilder stringBuilder = new StringBuilder("http://sis.51funmore.com:8081");//http://sis.51funmore.com:8081
+       stringBuilder.append(context.getServletContext().getContextPath());
         try {
             this.uriPrefix = new URI(stringBuilder.toString());
         } catch (URISyntaxException e) {
