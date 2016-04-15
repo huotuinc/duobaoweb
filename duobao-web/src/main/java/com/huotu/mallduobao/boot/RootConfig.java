0c8e78b8a87f71e87b2a325169c4286d349cdf4a
lgh <106517651@qq.com>
2016-04-15 15:06:57 +0800

    启用定时schedle

--- duobao-web/src/main/java/com/huotu/mallduobao/boot/RootConfig.java
+++ duobao-web/src/main/java/com/huotu/mallduobao/boot/RootConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by lgh on 2016/3/22.
@@ -19,6 +20,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value = "com.huotu.mallduobao.repository", repositoryFactoryBeanClass = ClassicsRepositoryFactoryBean.class)
 @ImportResource(value = {"classpath:spring-jpa.xml"})
 @Import({LoggingConfig.class, CommonClientSpringConfig.class})
+@EnableScheduling
 public class RootConfig {
 
 
